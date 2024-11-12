944f18c82f1f9768962103a9129937eaaa0ff417
siseldyke <siseldyke@gmail.com>
2024-11-12 16:07:43 -0500

    tracks displaying

--- src/components/TrackList.jsx
+++ src/components/TrackList.jsx
@@ -1,7 +1,7 @@
 const TrackList = (props) => {
     const tracks = props.trackList.map((track) =>(
          <a key={track._id} onClick={ () => props.updateSelected(track)}>
-         <li key={track._id}>{track.name}</li>
+         <li key={track._id}>{track.title}</li>
          </a>
         ));
     
